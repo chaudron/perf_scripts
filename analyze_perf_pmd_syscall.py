03e9a564de07975b56c88411a604989387c4c029
Eelco Chaudron <echaudro@redhat.com>
2022-03-16 15:36:39 +0100

    pmd_syscall: fix example to include the sys_exit event

--- analyze_perf_pmd_syscall.py
+++ analyze_perf_pmd_syscall.py
@@ -35,8 +35,8 @@
 #    Capture perf data for example from Open vSwitch's PMD threads with the
 #    following command:
 #
-#      perf record -e raw_syscalls:sys_enter -s --call-graph dwarf \
-#        --per-thread -g -i -t \
+#      perf record -e raw_syscalls:sys_enter -e raw_syscalls:sys_exit \
+#        -s --call-graph dwarf --per-thread -g -i -t \
 #        `ps -To tid,comm \`pidof ovs-vswitchd\` | \
 #        grep pmd | awk '{$1=$1};1' | cut -d " " -f 1 | xargs | \
 #        sed -e 's/ /,/g'`
